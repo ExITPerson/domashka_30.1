9828bd9dc7011eb4393b3cdfb439bfe1f75cb615
Ex_person <baranovs11@yandex.ru>
2025-09-28 02:36:54 +0300

    Materials

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    preview = models.ImageField(upload_to='preview/', verbose_name='preview')
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=50)
+    preview = models.ImageField(upload_to='preview/', verbose_name='preview')
+    description = models.TextField()
+    link_video = models.URLField(max_length=200)
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
--- users/models.py
+++ users/models.py
@@ -13,8 +13,8 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
 
     def __str__(self):
         return self.email
