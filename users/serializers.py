6c013176a6317503a3ea7011c41dae90a6f19c66
Ex_person <baranovs11@yandex.ru>
2025-10-04 01:01:15 +0300

    task 2

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from users.models import User
+from materials.models import Course, Lesson
+from users.models import User, Payments
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    course = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all())
+    lesson = serializers.PrimaryKeyRelatedField(queryset=Lesson.objects.all())
+
+    class Meta:
+        model = Payments
+        fields = ['id','payment_date', 'user', 'course', 'lesson', 'payment_amount']
