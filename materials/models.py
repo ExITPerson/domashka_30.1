cdd8c3d8f254070d2ac64e12af1dcfaaeaa233ba
Ex_person <baranovs11@yandex.ru>
2025-10-09 17:54:59 +0300

    task 3

--- materials/models.py
+++ materials/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+from config import settings
+
 
 class Course(models.Model):
     name = models.CharField(max_length=50)
     preview = models.ImageField(upload_to='preview/', verbose_name='preview', null=True, blank=True)
     description = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        verbose_name='Author',
+        related_name='course'
+    )
 
     class Meta:
         verbose_name = 'курс'
@@ -21,6 +31,14 @@ class Lesson(models.Model):
     description = models.TextField()
     link_video = models.URLField(max_length=200)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=None, blank=None)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        verbose_name='Author',
+        related_name='lesson'
+    )
 
     class Meta:
         verbose_name = 'урок'
--- materials/views.py
+++ materials/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course, Lesson
+from materials.permissions import AuthorOrStaff
 from materials.serializers import CourseSerializer, LessonSerializer, CountLessonSerializer
 from users.permissions import ModeratorsPermissions
 
@@ -17,38 +18,50 @@ class CourseViewSet(viewsets.ModelViewSet):
         return CourseSerializer
 
     def get_permissions(self):
-        if self.action == 'create' or self.action == 'destroy':
+        if self.action == 'create':
             self.permission_classes = [IsAuthenticated]
-        elif self.action == 'list' or self.action == 'retrieve' or self.action == 'update':
-            self.permission_classes = [IsAuthenticated, ModeratorsPermissions]
+        elif self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated, AuthorOrStaff]
+        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'list':
+            self.permission_classes = [IsAuthenticated, ModeratorsPermissions | AuthorOrStaff]
 
         return [permission() for permission in self.permission_classes]
+
+    def perform_create(self, serializer):
+        new_lesson = serializer.save()
+        new_lesson.author = self.request.user
+        new_lesson.save()
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        new_lesson = serializer.save()
+        new_lesson.author = self.request.user
+        new_lesson.save()
+
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, ModeratorsPermissions]
+    permission_classes = [IsAuthenticated, ModeratorsPermissions | AuthorOrStaff]
     queryset = Lesson.objects.all()
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, ModeratorsPermissions]
+    permission_classes = [IsAuthenticated, ModeratorsPermissions | AuthorOrStaff]
     queryset = Lesson.objects.all()
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, ModeratorsPermissions]
+    permission_classes = [IsAuthenticated, ModeratorsPermissions | AuthorOrStaff]
     queryset = Lesson.objects.all()
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, AuthorOrStaff]
     queryset = Lesson.objects.all()
